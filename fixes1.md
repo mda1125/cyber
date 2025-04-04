5d0288638b43c3bad6c67189ab3067bd20b158b1
Marc David <marc.c.david@gmail.com>
2025-04-04 08:33:38 -0700

    Added date and time

---
+++ fixes1.md
@@ -0,0 +1,3 @@
+# Site Fixes
+
+Under the Welcome Home, let's add the current date and time.  Each time the page is loaded, the current date and time is refreshed.
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // This script will be used to add interactivity to the website.
 // For example, it could be used to create a quiz that tests users' knowledge of cybersecurity.
 // It could also be used to create a chatbot that answers users' questions about cybersecurity.
+
+// Function to update the date and time
+function updateDateTime() {
+  const now = new Date();
+  const dateTimeString = now.toLocaleString(); // Format date and time based on locale
+  const dateTimeElement = document.getElementById('datetime');
+  if (dateTimeElement) {
+    dateTimeElement.textContent = dateTimeString;
+  }
+}
+
+// Call the function when the page loads
+window.onload = updateDateTime;
